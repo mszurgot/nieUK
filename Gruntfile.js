3a9ed9e1e2a614ba8463485753b31cd129f8ea7a
Mariusz <zloty11@gmail.com>
2015-01-03 23:29:20 +0100

    grunt
    działa konkatenacja plikow .css i .js
    oraz minifikacja .css .js .html
    projekt jest uporzadkowany tak zeby zmian dokonywać wyłącznie w folderze
    "src" i nie ruszać nic w "build". "Build" jest wersją produkcyjną i
    można ją najwyżej wyświetlać, bo wszelkie zmiany byłyby szybko nadpisane
    przy okazji aktualizacji w "src". Niestety nie jest jeszcz
    zautomatyzowane więc po każdej zmianie w plikach trzeba odpalić
    polecenie "grunt" przez cmd w glownym folderze projektu tzn tym gdzie
    znajduje sie plik Gruntfile.js
    wszystkie pliki js umieszczaj od tej pory w folderze src/js a po wydaniu
    polecenia "grunt" zostana one odpowiednio przetrawione i wyslane do
    "build/js". pliki html importują już przerobione pliki. Zarówno css jak
    i js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,75 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+	//łaczenie plików js w jeden o nazwie projektu z rozszerzeniem .js
+	 concat: {
+		jstarget: {
+		  src: 'src/js/*.js',
+		  dest: 'src/js/concatenated/<%= pkg.name %>.js'
+		}
+	  },
+	  //łaczenie plików css w jeden o nazwie projektu z rozszerzeniem .css
+	  concat_css: {
+		csstarget: {
+		  src: 'src/css/*.css',
+		  dest: 'src/css/concatenated/<%= pkg.name %>.css'
+		}
+	  },
+	//minimalizacja pliku .js po konkatenacji
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      jstarget: {
+        src: 'src/js/concatenated/<%= pkg.name %>.js',
+        dest: 'build/js/<%= pkg.name %>.min.js'
       }
-    }
+    },
+	//minimalizacja plików .css 
+	cssmin: {
+	  csstarget: {
+		files: [{ 
+		  expand: true,
+		  cwd: 'src/css/concatenated',
+		  src: '<%= pkg.name %>.css',
+		  dest: 'build/css',
+		  ext: '.min.css'
+		}]
+	  }
+	},
+	// minimalizacja htmla
+	htmlmin: { 
+		htmltarget: {
+		  options: {       
+			removeComments: true,
+			collapseWhitespace: true
+		  },
+		  files: [{                  
+			expand: true,
+			cwd: 'src/',    
+			src: '*.html', 
+			dest: 'build/'       
+		  }]
+		}
+	},
+	//obserwator
+	watch: {
+	  scripts: {
+		files: ['src/js/*.js'],
+		tasks: ['concat','uglify'],
+		options: {
+		  spawn: false,
+		}
+	  }
+	}
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat','concat_css','uglify','cssmin','htmlmin']);
 
 };
